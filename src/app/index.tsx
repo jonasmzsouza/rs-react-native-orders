3bc92621410714c5584d6616ac280754a3bef530
Jonas Souza <jonasmzsouza@gmail.com>
2024-02-09 15:30:41 -0300

    fix: type sectionlist, product title and use redirect expo-router

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import { useState, useRef } from "react";
 import { FlatList, SectionList, Text, View } from "react-native";
 import { Link } from "expo-router";
 import { Header } from "@/components/header";
-import { CATEGORIES, MENU } from "@/utils/data/products";
+import { CATEGORIES, MENU, ProductProps } from "@/utils/data/products";
 import { CategoryButton } from "@/components/category-button";
 import { Product } from "@/components/product";
 import { useCartStore } from "@/stores/cart-store";
@@ -11,7 +11,7 @@ export default function Home() {
   const cartStore = useCartStore();
   const [category, setCategory] = useState(CATEGORIES[0]);
 
-  const sectionListRef = useRef<SectionList>(null);
+  const sectionListRef = useRef<SectionList<ProductProps>>(null);
 
   const cartQuantityItems = cartStore.products.reduce(
     (total, product) => total + product.quantity,
--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,7 +1,7 @@
 import { Image, Text, View } from "react-native";
 import { useLocalSearchParams, useNavigation } from "expo-router";
 import { Feather } from "@expo/vector-icons";
-
+import { Redirect } from "expo-router";
 import { useCartStore } from "@/stores/cart-store";
 
 import { PRODUCTS } from "@/utils/data/products";
@@ -14,11 +14,17 @@ export default function Product() {
   const navigation = useNavigation();
   const { id } = useLocalSearchParams();
 
-  const product = PRODUCTS.filter((item) => item.id === id)[0];
+  const product = PRODUCTS.find((item) => item.id === id);
 
   function handleAddToCart() {
-    cartStore.add(product);
-    navigation.goBack();
+    if (product) {
+      cartStore.add(product); // ou product! e retirar do if
+      navigation.goBack();
+    }
+  }
+
+  if (!product) {
+    return <Redirect href="/" />;
   }
 
   return (
@@ -30,6 +36,8 @@ export default function Product() {
       />
 
       <View className="p-5 mt-8 flex-1">
+        <Text className="text-white text-xl font-heading">{product.title}</Text>
+
         <Text className="text-lime-400 text-2xl font-heading my-2">
           {formatCurrency(product.price)}
         </Text>
